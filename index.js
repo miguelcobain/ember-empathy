53a36a289347eee8796d17150bae95bbbfb2155b
Miguel Andrade <jmandrade0@gmail.com>
2017-01-27 16:43:07 -0800

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
 
+var quotes = [
+  {
+    text: 'As a tech community, we must treat documentation, marketing, logistics, infrastructure, art, etc. work with as much respect as engineering',
+    author: 'Yehuda Katz'
+  }, {
+    text: 'To maximize your impact in software, empower other developers.',
+    author: 'Yehuda Katz'
+  }
+];
+
 module.exports = {
-  name: 'ember-empathy'
+  name: 'ember-empathy',
+  preBuild: function() {
+    var quote = quotes[Math.floor(Math.random() * quotes.length)];
+    console.log('');
+    console.log('"' + quote.text + '"');
+    console.log('  — ' + quote.author);
+    console.log('');
+  }
 };
